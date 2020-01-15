db013161928a3e22f1e97aa0908af4ca8cf9ef99
Apersant1 <miracles.on.the.turn@gmail.com>
2020-01-15 22:31:12 +0300

    Added opportunity to create a new topic

--- Main/urls.py
+++ Main/urls.py
@@ -6,4 +6,6 @@ app_name = 'Main'
 urlpatterns = [
     path('', view_index, name='index'),
     re_path(r'^topics/$', get_all_topics, name='topics'),
+    re_path(r'^topics/(?P<topic_id>\d+)/$' , get_topic, name='topic'),
+    re_path(r'^create_topic/$', create_topic, name='new_topic')
 ]
--- Main/views.py
+++ Main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Topic
+from .forms import TopicForm
 # Create your views here.
 def view_index(requests):
     return render(requests, 'index.html')
@@ -12,3 +15,18 @@ def get_all_topics(requests):
 
     return render(requests, 'topics.html', context={'topics': topics})
 
+def get_topic(requests, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-data_added')
+    return render(requests, 'topic.html', context={'topic':topic, 'entries':entries})
+
+def create_topic(request):
+    print(request)
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('Main:topics'))
+    return render(request, 'new_topic.html', context={'form':form})
