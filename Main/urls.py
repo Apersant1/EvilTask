ac05b079d3e0d9819e9b1f89a5b562d8f2686ba6
Apersant1 <miracles.on.the.turn@gmail.com>
2020-01-16 19:48:09 +0300

    Added opportunity create edit entries

--- Main/urls.py
+++ Main/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     re_path(r'^topics/$', get_all_topics, name='topics'),
     re_path(r'^topics/(?P<topic_id>\d+)/$', get_topic, name='topic'),
     re_path(r'^create_topic/$', create_topic, name='new_topic'),
-    re_path(r'^create_entry/(?P<topic_id>\d+)/$', create_entry, name='create_entry')
+    re_path(r'^create_entry/(?P<topic_id>\d+)/$', create_entry, name='create_entry'),
+    re_path(r'^edit_entry/(?P<entry_id>\d+)/$', edit_entry, name='edit_entry'),
 
 
 ]
